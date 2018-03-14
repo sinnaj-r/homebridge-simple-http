011bbbec041bb4f9ef588190c3dd49d556837e6a
Jannis Rosenbaum <jannis.rosenbaum@traser-software.de>
2018-03-14 14:15:38 +0100

    Prettier

--- index.js
+++ index.js
@@ -1,118 +1,128 @@
-var http = require('http');
-var Accessory,
-    Service,
-    Characteristic,
-    UUIDGen;
+var http = require("http")
+var Accessory, Service, Characteristic, UUIDGen
 
-module.exports = function (homebridge) {
-    console.log("homebridge API version: " + homebridge.version);
+module.exports = function(homebridge) {
+    console.log("homebridge API version: " + homebridge.version)
 
     // Accessory must be created from PlatformAccessory Constructor
-    Accessory = homebridge.platformAccessory;
+    Accessory = homebridge.platformAccessory
 
     // Service and Characteristic are from hap-nodejs
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    UUIDGen = homebridge.hap.uuid;
+    Service = homebridge.hap.Service
+    Characteristic = homebridge.hap.Characteristic
+    UUIDGen = homebridge.hap.uuid
 
     // For platform plugin to be considered as dynamic platform plugin,
     // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic
     // must be true
-    homebridge.registerPlatform("homebridge-samplePlatform", "SamplePlatform", SamplePlatform, true);
+    homebridge.registerPlatform(
+        "homebridge-samplePlatform",
+        "SamplePlatform",
+        SamplePlatform,
+        true
+    )
 }
 
 // Platform constructor config may be null api may be null if launched from old
 // homebridge version
 function SamplePlatform(log, config, api) {
-    log("SamplePlatform Init");
-    var platform = this;
-    this.log = log;
-    this.config = config;
-    this.accessories = [];
+    log("SamplePlatform Init")
+    var platform = this
+    this.log = log
+    this.config = config
+    this.accessories = []
 
-    this.requestServer = http.createServer(function (request, response) {
-        if (request.url === "/add") {
-            this.addAccessory(new Date().toISOString());
-            response.writeHead(204);
-            response.end();
-        }
+    this.requestServer = http.createServer(
+        function(request, response) {
+            if (request.url === "/add") {
+                this.addAccessory(new Date().toISOString())
+                response.writeHead(204)
+                response.end()
+            }
 
-        if (request.url == "/reachability") {
-            this.updateAccessoriesReachability();
-            response.writeHead(204);
-            response.end();
-        }
+            if (request.url == "/reachability") {
+                this.updateAccessoriesReachability()
+                response.writeHead(204)
+                response.end()
+            }
 
-        if (request.url == "/remove") {
-            this.removeAccessory();
-            response.writeHead(204);
-            response.end();
-        }
-    }.bind(this));
+            if (request.url == "/remove") {
+                this.removeAccessory()
+                response.writeHead(204)
+                response.end()
+            }
+        }.bind(this)
+    )
 
-    this
-        .requestServer
-        .listen(18081, function () {
-            platform.log("Server Listening...");
-        });
+    this.requestServer.listen(18081, function() {
+        platform.log("Server Listening...")
+    })
 
     if (api) {
         // Save the API object as plugin needs to register new accessory via this object
-        this.api = api;
+        this.api = api
 
         // Listen to event "didFinishLaunching", this means homebridge already finished
         // loading cached accessories. Platform Plugin should only register new
         // accessory that doesn't exist in homebridge after this event. Or start
         // discover new accessories.
-        this
-            .api
-            .on('didFinishLaunching', function () {
-                platform.log("DidFinishLaunching");
-            }.bind(this));
+        this.api.on(
+            "didFinishLaunching",
+            function() {
+                platform.log("DidFinishLaunching")
+            }.bind(this)
+        )
     }
 }
 
 // Function invoked when homebridge tries to restore cached accessory. Developer
 // can configure accessory at here (like setup event handler). Update current
 // value.
-SamplePlatform.prototype.configureAccessory = function (accessory) {
-    this.log(accessory.displayName, "Configure Accessory");
-    var platform = this;
+SamplePlatform.prototype.configureAccessory = function(accessory) {
+    this.log(accessory.displayName, "Configure Accessory")
+    var platform = this
 
     // Set the accessory to reachable if plugin can currently process the accessory,
     // otherwise set to false and update the reachability later by invoking
     // accessory.updateReachability()
-    accessory.reachable = true;
+    accessory.reachable = true
 
-    accessory.on('identify', function (paired, callback) {
-        platform.log(accessory.displayName, "Identify!!!");
-        callback();
-    });
+    accessory.on("identify", function(paired, callback) {
+        platform.log(accessory.displayName, "Identify!!!")
+        callback()
+    })
 
     if (accessory.getService(Service.Lightbulb)) {
         accessory
             .getService(Service.Lightbulb)
             .getCharacteristic(Characteristic.On)
-            .on('set', function (value, callback) {
-                platform.log(accessory.displayName, "Light -> " + value);
-                callback();
-            });
+            .on("set", function(value, callback) {
+                platform.log(accessory.displayName, "Light -> " + value)
+                callback()
+            })
     }
 
-    this
-        .accessories
-        .push(accessory);
+    this.accessories.push(accessory)
 }
 
 // Handler will be invoked when user try to config your plugin. Callback can be
 // cached and invoke when necessary.
-SamplePlatform.prototype.configurationRequestHandler = function (context, request, callback) {
-    this.log("Context: ", JSON.stringify(context));
-    this.log("Request: ", JSON.stringify(request));
+SamplePlatform.prototype.configurationRequestHandler = function(
+    context,
+    request,
+    callback
+) {
+    this.log("Context: ", JSON.stringify(context))
+    this.log("Request: ", JSON.stringify(request))
 
     // Check the request response
-    if (request && request.response && request.response.inputs && request.response.inputs.name) {
-        this.addAccessory(request.response.inputs.name);
+    if (
+        request &&
+        request.response &&
+        request.response.inputs &&
+        request.response.inputs.name
+    ) {
+        this.addAccessory(request.response.inputs.name)
 
         // Invoke callback with config will let homebridge save the new config into
         // config.json Callback = function(response, type, replace, config) set "type"
@@ -120,10 +130,10 @@ SamplePlatform.prototype.configurationRequestHandler = function (context, reques
         // to true will let homebridge replace existing config in config.json "config"
         // is the data platform trying to save
         callback(null, "platform", true, {
-            "platform": "SamplePlatform",
-            "otherConfig": "SomeData"
-        });
-        return;
+            platform: "SamplePlatform",
+            otherConfig: "SomeData"
+        })
+        return
     }
 
     // - UI Type: Input Can be used to request input from user User response can be
@@ -131,14 +141,14 @@ SamplePlatform.prototype.configurationRequestHandler = function (context, reques
     // configurationRequestHandler being invoked
 
     var respDict = {
-        "type": "Interface",
-        "interface": "input",
-        "title": "Add Accessory",
-        "items": [
+        type: "Interface",
+        interface: "input",
+        title: "Add Accessory",
+        items: [
             {
-                "id": "name",
-                "title": "Name",
-                "placeholder": "Fancy Light"
+                id: "name",
+                title: "Name",
+                placeholder: "Fancy Light"
             } //,
             // {   "id": "pw",   "title": "Password",   "secure": true }
         ]
@@ -158,26 +168,26 @@ SamplePlatform.prototype.configurationRequestHandler = function (context, reques
     // image data",   "showActivityIndicator": true, "showNextButton": true,
     // "buttonText": "Login in browser", "actionURL": "https://google.com" } Plugin
     // can set context to allow it track setup process
-    context.ts = "Hello";
+    context.ts = "Hello"
 
     // Invoke callback to update setup UI
-    callback(respDict);
+    callback(respDict)
 }
 
 // Sample function to show how developer can add accessory dynamically from
 // outside event
-SamplePlatform.prototype.addAccessory = function (accessoryName) {
-    this.log("Add Accessory");
-    var platform = this;
-    var uuid;
+SamplePlatform.prototype.addAccessory = function(accessoryName) {
+    this.log("Add Accessory")
+    var platform = this
+    var uuid
 
-    uuid = UUIDGen.generate(accessoryName);
+    uuid = UUIDGen.generate(accessoryName)
 
-    var newAccessory = new Accessory(accessoryName, uuid);
-    newAccessory.on('identify', function (paired, callback) {
-        platform.log(accessory.displayName, "Identify!!!");
-        callback();
-    });
+    var newAccessory = new Accessory(accessoryName, uuid)
+    newAccessory.on("identify", function(paired, callback) {
+        platform.log(accessory.displayName, "Identify!!!")
+        callback()
+    })
     // Plugin can save context on accessory to help restore accessory in
     // configureAccessory() newAccessory.context.something = "Something" Make sure
     // you provided a name for service, otherwise it may not visible in some HomeKit
@@ -185,34 +195,36 @@ SamplePlatform.prototype.addAccessory = function (accessoryName) {
     newAccessory
         .addService(Service.Lightbulb, "Test Light")
         .getCharacteristic(Characteristic.On)
-        .on('set', function (value, callback) {
-            platform.log(accessory.displayName, "Light -> " + value);
-            callback();
-        });
+        .on("set", function(value, callback) {
+            platform.log(accessory.displayName, "Light -> " + value)
+            callback()
+        })
 
-    this
-        .accessories
-        .push(newAccessory);
-    this
-        .api
-        .registerPlatformAccessories("homebridge-samplePlatform", "SamplePlatform", [newAccessory]);
+    this.accessories.push(newAccessory)
+    this.api.registerPlatformAccessories(
+        "homebridge-samplePlatform",
+        "SamplePlatform",
+        [newAccessory]
+    )
 }
 
-SamplePlatform.prototype.updateAccessoriesReachability = function () {
-    this.log("Update Reachability");
+SamplePlatform.prototype.updateAccessoriesReachability = function() {
+    this.log("Update Reachability")
     for (var index in this.accessories) {
-        var accessory = this.accessories[index];
-        accessory.updateReachability(false);
+        var accessory = this.accessories[index]
+        accessory.updateReachability(false)
     }
 }
 
 // Sample function to show how developer can remove accessory dynamically from
 // outside event
-SamplePlatform.prototype.removeAccessory = function () {
-    this.log("Remove Accessory");
-    this
-        .api
-        .unregisterPlatformAccessories("homebridge-samplePlatform", "SamplePlatform", this.accessories);
+SamplePlatform.prototype.removeAccessory = function() {
+    this.log("Remove Accessory")
+    this.api.unregisterPlatformAccessories(
+        "homebridge-samplePlatform",
+        "SamplePlatform",
+        this.accessories
+    )
 
-    this.accessories = [];
+    this.accessories = []
 }
