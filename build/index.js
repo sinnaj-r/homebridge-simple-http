ade27d8efe4611feacedcb45d73849c39b0b000d
Jannis Rosenbaum <jannis.rosenbaum@traser-software.de>
2018-03-22 18:01:29 +0100

    Added all build files to git (for faster deployment)

--- build/index.js
+++ build/index.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 }
 var SimpleHTTPSwitch_1 = __importDefault(require("./SimpleHTTPSwitch"));
-var Service, Characteristic;
 module.exports = function (homebridge) {
     var hap = homebridge.hap;
     Service = hap.Service;
---
+++ build/test.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+var SimpleHTTPSwitch_1 = __importDefault(require("./SimpleHTTPSwitch"));
+var example_config = {
+    status_url: "http://192.168.1.14:8081/status/power_stat",
+    set_on_url: "http://192.168.1.14:8081/send/KEY_POWER",
+    set_off_url: "http://192.168.1.14:8081/send/KEY_POWER2",
+    on_if_this_fn: "(obj)=>obj.status ? obj.status=='ON' : null",
+    name: "Anlage"
+    //on_if_this: {status:"ON"},
+    //off_if_this: {status:"OFF"},
+};
+var SHS = new SimpleHTTPSwitch_1.default(function (str) { return console.log(str); }, example_config);
+SHS.getPowerState(function (err, state) {
+    console.log("State:", state);
+    SHS.setPowerState(false, function (err) { return console.log(err); });
+});
+setTimeout(function () {
+    SHS.getPowerState(function (err, state) {
+        console.log("State:", state);
+        SHS.setPowerState(true, function (err) { return console.log(err); });
+    });
+}, 3000);
+//# sourceMappingURL=test.js.map
